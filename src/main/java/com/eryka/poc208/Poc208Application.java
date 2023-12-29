2e6da426939539c9971111a2c91cb87f47adf48e
KEWG4 <kewg.ecommerce@gmail.com>
2023-12-29 18:11:42 -0300

    commit inicial

---
+++ src/main/java/com/eryka/poc208/Poc208Application.java
@@ -0,0 +1,13 @@
+package com.eryka.poc208;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Poc208Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Poc208Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/eryka/poc208/Poc208ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eryka.poc208;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Poc208ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
